c7641d3e94c041a94f019a70aceaf22081a686a9
Sang jin Lee <w1004123@gmail.com>
2023-01-12 10:45:12 +0900

    commit

---
+++ 2023-Hackathon/src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import Header from './Components/Header';
+import Body from './Components/Body';
+import Star from './Components/Star';
+import Nostar from './Components/Nostar';
+import Bodycap from './Components/Bodycap';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+      <>
+        <Header/>
+        <Routes>
+          <Route path="/" element={ 
+                <>
+                  <Body/>
+                </>
+              }
+            />
+          <Route path="/star in" element={ 
+                <>
+                  <Star/>
+                </>
+              }
+            />
+          <Route path="/nostar in" element={ 
+                <>
+                  <Nostar/>
+                </>
+              }
+            />
+            <Route path="/body cap" element={ 
+                <>
+                  <Bodycap/>
+                </>
+              }
+            />
+          </Routes>
+        </>
+      </BrowserRouter>
+    </div> 
+  );
+}
+
+export default App;
---
+++ 2023-Hackathon/src/Components/Body.js
@@ -0,0 +1,15 @@
+import './css/Body.css';
+import camera from '../assets/images/Vector.png';
+
+function Body() {
+    return(
+        <div className="center">
+            <div className="imgFile">
+            <img src={camera} class="img5" alt="Big"/>
+            </div>
+            <button class="button">오늘의 코디 하러 가기!</button>
+        </div>
+    );
+}
+
+export default Body;
